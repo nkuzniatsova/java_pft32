5ecedabb478fdca7837cb65a4330c4d24ac799d3
Natallia Kuzniatsova <n_kuzniatsova@yahoo.com>
2016-09-06 03:00:55 +0200

    Подключен тестфреймворк TestNG и создан первый тест
    Подключен тестфреймворк TestNG и создан первый тест PointTests

---
+++ speltuin/src/test/java/ptf32/speltuin/PointTests.java
@@ -0,0 +1,19 @@
+package ptf32.speltuin;
+
+import Lesson1.PointWithMethod;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Natallia on 6-9-2016.
+ */
+public class PointTests {
+@Test
+public void testArea() {
+  PointWithMethod p1 = new PointWithMethod(1,5);
+  PointWithMethod p2 = new PointWithMethod(1,3);
+  /* assert p1.distance(p2) == 2; */
+  Assert.assertEquals(p1.distance(p2), 2.0);
+
+  }
+}
